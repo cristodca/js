82c768a877d8099cb32b885748e4cf6d478ed678
@cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2021-01-09 14:29:38 -0600

    Clase 1 - Variables

---
+++ js/clase1.js
@@ -0,0 +1,13 @@
+// CLASE 1
+
+// Clase 1 - Variables
+
+// console.log("Hello world!")
+
+var myName = "Cristopher", myLastName = "D. Chavez"
+var myAge = 20, myWeight = 60
+
+// console.log(`Hi, ${myName} ${myLastName}`)
+// console.log(`I'm ${myAge} years old`)
+
+
