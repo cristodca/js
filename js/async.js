06e4579cb6e7b1c5e8676ef0c9a07a40af63ec1f
@cristodca (Cristopher D. Chavez) <cristodca.studio@gmail.com>
2021-01-11 22:50:49 -0600

    Clase 38 - Memorización

--- js/async.js
+++ js/async.js
@@ -292,3 +292,27 @@ function divisionEntera(dividendo, divisor) {
 
     return 1 + divisionEntera(dividendo - divisor, divisor)
 }
+
+
+// ===============
+// Clase 38 - Memorización
+// Guardar en memoria para acceder rápidamente a este dato posteriormente
+
+function factorial(n) {
+    if (!this.cache){
+        this.cache = {}
+    }
+
+    // debugger
+    if (this.cache[n]) {
+        return this.cache[n]
+    }
+
+    if (n === 1) {
+        return 1
+    }
+
+    this.cache[n] = n * factorial(n - 1)
+    // debugger
+    return this.cache[n]
+}
